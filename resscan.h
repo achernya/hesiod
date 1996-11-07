99d04160a359941a973c3942435ac4be9ae9bc48
Greg Hudson <ghudson@mit.edu>
1996-11-07 02:43:27 +0000

    Added MIT copyright and switched to standard RCS keyword convention. Code now follows Athena coding standards. DATASIZE is now an arbitrary value (8192) instead of being derived  from PACKETSZ.  PACKETSZ is simply the DNS datagram transport limit  and does not limit the size of a DNS answer (if, for instance, the  implementation uses a virtual circuit).
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk/athena/lib/hesiod@9316 728af825-273c-0410-89f9-f7d3b574a069

--- resscan.h
+++ resscan.h
@@ -1,54 +1,47 @@
-/* This file contains definitions for the name resolver scanning routines
- * used by the Hesiod library.
+/* $Id: resscan.h,v 1.6 1996-11-07 02:43:27 ghudson Exp $ */
+
+/* Copyright 1988, 1996 by the Massachusetts Institute of Technology.
  *
- * For copying and distribution information, see the file <mit-copyright.h>.
- *
- * Original version by Steve Dyer, IBM/Project Athena, and Sam Hsu,
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in
+ * advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.
+ * M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ */
+
+/* Original version by Steve Dyer, IBM/Project Athena, and Sam Hsu,
  * DEC/Project Athena.
- *
- *	$Author: cfields $
- *	$Athena: resscan.h,v 1.3 88/08/07 21:53:09 treese Locked $
- *	$Header: /afs/dev.mit.edu/source/repository/athena/lib/hesiod/resscan.h,v 1.5 1995-06-25 01:36:21 cfields Exp $
- *	$Source: /afs/dev.mit.edu/source/repository/athena/lib/hesiod/resscan.h,v $
- *	$Log: not supported by cvs2svn $
- * Revision 1.4  1988/08/07  23:17:42  treese
- * Second-public-distribution
- *
- * Revision 1.3  88/08/07  21:53:09  treese
- * First public distribution
- * 
- * Revision 1.2  88/06/12  00:53:14  treese
- * Cleaned up to work with Saber.
- * First public distribution.
- * 
- * Revision 1.2  88/06/05  19:51:32  treese
- * Cleaned up for public distribution
- * 
+ */
+
+/* This file is part of the Hesiod library.  It contains definitions
+ * for the name resolver scanning routines used by the library.
  */
 
 typedef struct rr {
-    short type;			     /* RR type */
-    short class;		     /* RR class */
-    int dlen;			     /* len of data section */
-    char *data;			     /* pointer to data */
-} rr_t, *rr_p;
+  short type;			/* RR type */
+  short class;			/* RR class */
+  int dlen;			/* len of data section */
+  const char *data;		/* pointer to data */
+} rr_t;
 
 typedef struct nsmsg {
-    int len;			     /* sizeof(msg) */
-    int ns_off;			     /* offset to name server RRs */
-    int ar_off;			     /* offset to additional RRs */
-    int count;			     /* total number of RRs */
-    HEADER *hd;			     /* message header */
-    rr_t rr;			     /* vector of (stripped-down) RR descriptors */
-} nsmsg_t, *nsmsg_p;
+  int len;			/* sizeof(msg) */
+  int ns_off;			/* offset to name server RRs */
+  int ar_off;			/* offset to additional RRs */
+  int count;			/* total number of RRs */
+  HEADER *hd;			/* message header */
+  rr_t rr;			/* vector of (stripped-down) RR descriptors */
+} nsmsg_t;
 
-typedef struct retransXretry {
-    short retrans;
-    short retry;
-} retransXretry_t, *retransXretry_p;
+#define DATASIZE 8192
+#define NMSGSIZE (sizeof(nsmsg_t) + \
+		  sizeof(rr_t) * (DATASIZE / RRFIXEDSZ))
 
-
-#define RES_INITCHECK() if(!(_res.options&RES_INIT))res_init();
-
-extern char *p_cdname(), *p_rr(), *p_type(), *p_class();
-extern struct nsmsg *res_scan(), *resolve(), *_resolve(); 
+nsmsg_t *hes__resolve(const char *name, int class, int type, char *nmsgbuf,
+		      char *databuf);
