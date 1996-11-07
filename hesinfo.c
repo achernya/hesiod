ffe53b0474a94e93310408d4dbc837fb6f1a55ae
Greg Hudson <ghudson@mit.edu>
1996-11-07 02:25:05 +0000

    Added MIT copyright and switched to standard RCS keyword convention. Code now follows Athena coding standards. Use new reentrant interfaces.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk/athena/lib/hesiod@9309 728af825-273c-0410-89f9-f7d3b574a069

--- hesinfo.c
+++ hesinfo.c
@@ -1,97 +1,94 @@
-/* This is the source code for the hesinfo program, used to test the
- * Hesiod name server.
+/* Copyright 1988, 1996 by the Massachusetts Institute of Technology.
  *
- *	$Source: /afs/dev.mit.edu/source/repository/athena/lib/hesiod/hesinfo.c,v $
- *	$Author: probe $
- *	$Athena: hesinfo.c,v 1.4 88/08/07 21:52:19 treese Locked $
- *	$Log: not supported by cvs2svn $
- * Revision 1.5  88/08/07  23:16:50  treese
- * Second-public-distribution
- * 
- * Revision 1.4  88/08/07  21:52:19  treese
- * First public distribution
- * 
- * Revision 1.3  88/06/12  00:52:34  treese
- * Cleaned up to work with Saber.
- * First public distribution.
- * 
- * Revision 1.2  88/06/05  19:51:18  treese
- * Cleaned up for public distribution
- * 
- *
- * Copyright 1988 by the Massachusetts Institute of Technology.  See the
- * file <mit-copyright.h> for copying and distribution information.
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in
+ * advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.
+ * M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
  */
 
-#include "mit-copyright.h"
+/* This file is a simple driver for the Hesiod library. */
 
-#ifndef lint
-static char rcsid_hesinfo_c[] = "$Header: /afs/dev.mit.edu/source/repository/athena/lib/hesiod/hesinfo.c,v 1.6 1991-01-21 12:35:27 probe Exp $";
-#endif
+static char rcsid[] = "$Id: hesinfo.c,v 1.7 1996-11-07 02:25:05 ghudson Exp $";
 
 #include <stdio.h>
-#include <hesiod.h>
+#include "hesiod.h"
 
-main(argc, argv)
-char *argv[];
+extern int optind;
+
+int main(int argc, char **argv)
 {
-	register char *cp, **cpp;
-	char *hes_to_bind(), **hes_resolve();
-	int lflag = 0, errflg = 0, bflag = 0;
-	extern int optind;
-	char *identifier, *type;
-	int c;
-	
-	while ((c = getopt(argc, argv, "lb")) != EOF) {
-		if (c == 'l') lflag = 1;
-		else if (c == 'b') bflag = 1;
-		else errflg++;
-	}
-	if (argc - optind != 2 || errflg) {
-		fprintf(stderr,"Usage: %s [-bl] identifier type\n",argv[0]);
-		fprintf(stderr,"	-l selects long format\n");
-		fprintf(stderr,"	-b also does hes_to_bind conversion\n");
-		exit(2);
-	}
-	identifier = argv[optind];
-	type = argv[optind+1];
-		
-	if (bflag) {
-		if (lflag)
-			printf("hes_to_bind(%s, %s) expands to\n", 
-			       identifier, type);
-		cp = hes_to_bind(identifier, type);
-		if (cp == NULL) {
-			printf(" error %d\n", hes_error());
-			exit(1);
-		} 
-		printf("%s\n", cp);
-		if (lflag) printf("which ");
-	}
-	if (lflag) 
-		printf("resolves to\n");
+  char errbuf[HES_MAX_ERRLEN], buf[1024], *vec[128], **p, *identifier, *type;
+  int lflag = 0, errflg = 0, bflag = 0, c, status;
 
-	cpp = hes_resolve(identifier, type);
-	if (cpp == NULL) { 
-		if (lflag) printf("nothing\n");
-		switch(hes_error()) {
-		case 0:
-			break;
-		case HES_ER_NOTFOUND:
-			fputs("Hesiod name not found\n", stderr);
-			break;
-		case HES_ER_CONFIG:
-			fputs("Hesiod configuration error\n", stderr);
-			break;
-		default:
-			fputs("Unknown Hesiod error\n", stderr);
-			break;
-		}
-	} else {
-		while(*cpp) printf("%s\n", *cpp++);
+  while ((c = getopt(argc, argv, "lb")) != EOF)
+    {
+      switch (c)
+	{
+	case 'l':
+	  lflag = 1;
+	  break;
+	case 'c':
+	  bflag = 1;
+	  break;
+	default:
+	  errflg++;
 	}
-	if (!cpp)
-		 exit(1);
-	else
-		exit(0);
+    }
+  if (argc - optind != 2 || errflg)
+    {
+      fprintf(stderr,"Usage: %s [-bl] identifier type\n",argv[0]);
+      fprintf(stderr,"	-l selects long format\n");
+      fprintf(stderr,"	-b also does hes_to_bind conversion\n");
+      return 2;
+    }
+
+  identifier = argv[optind];
+  type = argv[optind + 1];
+
+  hes_init();
+
+  /* Display bind name if requested. */
+  if (bflag)
+    {
+      if (lflag)
+	printf("hes_to_bind(%s, %s) expands to\n", identifier, type);
+      status = hes_to_bind_r(identifier, type, buf, sizeof(buf));
+      if (status != HES_ER_OK)
+	{
+	  if (lflag)
+	    printf("nothing\n");
+	  hes_strerror_r(status, errbuf, sizeof(errbuf));
+	  fprintf(stderr, "%s\n", errbuf);
+	  exit(1);
+	}
+      printf("%s\n", buf);
+      if (lflag)
+	printf("which ");
+    }
+
+  if (lflag) 
+    printf("resolves to\n");
+
+  /* Do the hesiod resolve and check for errors. */
+  status = hes_resolve_r(identifier, type, vec, sizeof(vec));
+  if (status != HES_ER_OK)
+    { 
+      if (lflag)
+	printf("nothing\n");
+      hes_strerror_r(status, errbuf, sizeof(errbuf));
+      fprintf(stderr, "%s\n", errbuf);
+      return 1;
+    }
+
+  /* Display the results. */
+  for (p = vec; *p; p++)
+    printf("%s\n", *p);
+  return 0;
 }
