3ad130f8cc31b79e3900168a192e5f5a87e818e2
Greg Hudson <ghudson@mit.edu>
1996-12-08 21:25:27 +0000

    Add new passwd field extensions.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk/athena/lib/hesiod@9404 728af825-273c-0410-89f9-f7d3b574a069

--- acconfig.h
+++ acconfig.h
@@ -1,5 +1,14 @@
 /* Define if struct passwd has a field pw_quota. */
 #undef HAVE_PW_QUOTA
 
-/* Deifne if struct passwd has a field pw_comment. */
+/* Define if struct passwd has a field pw_comment. */
 #undef HAVE_PW_COMMENT
+
+/* Define if struct passwd has a field pw_change. */
+#undef HAVE_PW_CHANGE
+
+/* Define if struct passwd has a field pw_class. */
+#undef HAVE_PW_CLASS
+
+/* Define if struct passwd has a field pw_expire. */
+#undef HAVE_PW_EXPIRE
